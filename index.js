dfc72edfdfc6a6703bef959a543b1b10c77f267c
DatNT76 <tiendat4771@gmail.com>
2020-07-28 07:13:24 +0700

    add jenkins file

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 
 const app = express();
 
-// Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
     return res.send("Hello World");
 });
